a0b8801e82f4624249e35afe71858049d1995869
Eduardo Pereira <augusto.pereira@protonmail.com>
2021-08-10 16:14:22 -0300

    build: routes initial structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Routes } from "./routes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/routes/index.jsx
@@ -0,0 +1,4 @@
+import { Switch, Route } from "react-router";
+export const Routes = () => (
+  <Switch>{/* Insert the Routes Here With the Pages */}</Switch>
+);

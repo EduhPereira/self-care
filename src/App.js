4884f55a2d10a69113196cecc4f92d71aad637b5
Odair Lopes <odairlopesdev@gmail.com>
2021-08-12 15:09:07 -0300

    feat: add component modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 import { Routes } from "./routes";
+import { useState } from "react";
+import { Modal } from "./components/Modal";
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <div className="App">
+      {/* {isModalVisible ? (<Modal onClose={() => setIsModalVisible(false)}> Aqui entra os 
+      inputs e componentes dos childrens</Modal>) : null} */}
       <Routes />
     </div>
   );

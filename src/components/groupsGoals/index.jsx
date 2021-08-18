f2236097ddd558196fa903001ae6727d223fce7e
Eduardo Pereira <augusto.pereira@protonmail.com>
2021-08-18 14:28:06 -0300

    feat: add component group goals into group page

--- src/components/groupsGoals/index.jsx
+++ src/components/groupsGoals/index.jsx
@@ -4,20 +4,21 @@ import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
 import { useUser } from "../../providers/UserProvider";
+import { toast } from "react-toastify";
 
-export const GroupGoals = () => {
+export const GroupGoals = ({ GroupId }) => {
+  const [updater, setUpdater] = useState(0);
   const [goalsList, setGoalsList] = useState([]);
   const [page, setPage] = useState(1);
-  const [group, setGroup] = useState(4);
+  const [group, setGroup] = useState(GroupId);
   const { id, token } = useUser();
 
-  //Busca das metas já existentes no grupo
   useEffect(() => {
     api
       .get(`/goals/?group=${group}&page=${page}`)
       .then((response) => setGoalsList(response.data.results))
       .catch((e) => console.log(e.message));
-  }, [page]);
+  }, [page, updater]);
 
   const schema = yup.object().shape({
     title: yup.string().required("O Campo é Obrigatório"),
@@ -32,12 +33,11 @@ export const GroupGoals = () => {
     resolver: yupResolver(schema),
   });
 
-  //Requisição para a adição de uma nova meta
   const onSubmit = ({ title, difficulty }) => {
     const newGoal = {
       title: title,
       difficulty: difficulty,
-      how_much_achieved: 100,
+      how_much_achieved: 0,
       group: group,
     };
     api
@@ -46,11 +46,10 @@ export const GroupGoals = () => {
           Authorization: `Bearer ${token}`,
         },
       })
-      .then() //Espaço para inserir um toast sinalizando o sucesso ao adicionar uma nova meta
-      .catch(); //Tratamento de Erro ao adicionar uma nova meta
+      .then(toast.success("Nova Meta Adicionada"))
+      .catch();
   };
 
-  //Funções para paginação das metas
   const handlePrevious = () => {
     if (page > 1) {
       setPage(page - 1);
@@ -89,16 +88,18 @@ export const GroupGoals = () => {
         <button type="submit">Adicionar</button>
       </form>
 
-      {/* listagem de metas já criadas, porém sem folha de estilo ou componentes para cada meta */}
       <h2>Metas do Grupo</h2>
       {goalsList.map((goal) => {
         return (
-          <div>
-            <span>{goal.title}</span>
-            <span>{goal.difficulty}</span>
-            <span>{goal.how_much_achieved}</span>
-            <button>Atualizar / Editar</button>
-            <button>Deletar</button>
+          <div key={goal.id}>
+            <div>{goal.title}</div>
+            <div>{goal.difficulty}</div>
+            <div>{goal.how_much_achieved}</div>
+            <div>
+              <button>Editar</button>
+              <button>Deletar</button>
+              <button>Check</button>
+            </div>
           </div>
         );
       })}

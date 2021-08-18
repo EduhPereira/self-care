477d6a9792dd90961b8f4335a0579cd14c4919dc
Eduardo Pereira <augusto.pereira@protonmail.com>
2021-08-18 10:25:10 -0300

    feat: add toast structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
 import { Routes } from "./routes";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer
+        position="top-right"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Routes />
     </div>
   );
